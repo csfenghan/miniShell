7efd5995598bd764ba7858276fc7c0ab774572e6
fenghan <csfenghan@outlook.com>
2021-02-25 19:29:36 +0800

    添加了run_cmd

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BIN_PATH	(./bin)		//执行文件搜索路径
 #define BUF_SIZE	1024		//输入缓存大小
 #define MAX_CMD_NUM	64			//最大支持的命令数
 #define MAX_CMD_LEN	64			//单个命令的最大长度
 
-void parser_args(char *,char **);
+void parser_cmd(char *,char **);
+void run_cmd(char **);
 
 int main(int argc,char **argv)
 {
@@ -25,18 +27,13 @@ int main(int argc,char **argv)
 		if(fgets(input_buf,BUF_SIZE,stdin)==NULL){
 			perror("fgets error");
 		}
-		parser_args(input_buf,cmd_args);
-		for(int i=0;i<MAX_CMD_NUM;i++){
-			if(cmd_args[i][0]==0)
-				break;
-			printf("%s ",cmd_args[i]);
-		}
-		printf("\n");
+		parser_cmd(input_buf,cmd_args);
+		run_cmd(cmd_args);
 	}
 }
 
-
-void parser_args(char *input,char** output)
+////////////////////////////////////////////////////////////////
+void parser_cmd(char *input,char** output)
 {
 	char *ptr=input,last_char=' ';
 	int i=0,j=0;
@@ -59,5 +56,31 @@ void parser_args(char *input,char** output)
 			j=0;
 		}
 		last_char=*ptr++;
+	}
+}
+
+void run_cmd(char **cmd_args)
+{
+	if(strcmp(cmd_args[0],"cd")==0){
+		if(cmd_args[2][0]!=0){
+			fprintf(stderr,"minibash: cd : too many arguments\n");
+			return;
+		}
+		if(chdir(cmd_args[1])<0){
+			perror("cd error");
+			return;
+		}	
+	}
+	else if(strcmp(cmd_args[0],"pwd")==0){
+		if(cmd_args[1][0]!=0){
+			fprintf(stderr,"minibash: pwd : too many arguments\n");
+			return;
+		}
+		char buf[BUF_SIZE];
+		if(getcwd(buf,BUF_SIZE)==NULL){
+			perror("pwd error");
+			return;
+		}
+		printf("%s\n",buf);
 	}
 }
