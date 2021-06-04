b624910556ac7fa282874db8ffa7d386b538cbfe
fenghan <csfenghan@outlook.com>
2021-06-04 21:09:58 +0800

    实现了支持外部命令的多重管道

--- main.c
+++ main.c
@@ -4,74 +4,153 @@
 #include "signal_handler.h"
 #include "unix_api.h"
 
-// execute the command accordign to struct cmd_list
-void exec_cmd(struct cmd_list *cmd_list) {
+int pipfd[2];
+void process_builtin_command(struct cmd *cmd) {
+        sigset_t mask_all, mask_prev;
         int fd_in, fd_out;
-        int pipfd[2];
-        pid_t pid;
-        sigset_t mask_all, mask_chld, prev_mask;
-        struct cmd *cmd;
 
+        // block all signals when manipulate the global varible pipfd[2]
         sigfillset(&mask_all);
-        sigemptyset(&mask_chld);
-        sigaddset(&mask_chld, SIGCHLD);
+        sigprocmask(SIG_BLOCK, &mask_all, &mask_prev);
 
         // save the descriptor standard output and standard input
         fd_in = dup(STDIN_FILENO);
         fd_out = dup(STDOUT_FILENO);
 
-        // exec the command
-        for (cmd = cmd_list->head; cmd != NULL; cmd = cmd->next) {
-                // built-in command are not require fork a child process
-                if (cmd->cmd_type == CMD_POSITION_BUILTIN) {
-                        // set the input source according to the type of the previous cmd
-                        switch (cmd->prev_special_type) {
-                        case CMD_SPECIAL_AND:
-                        case CMD_SPECIAL_DEFAULT:
-                                // if the type is CMD_SPECIAL_AND or CMD_SPECIAL_DEFAULT,do nothing
-                                break;
-                        case CMD_SPECIAL_PIPE:
-                                // if the type is CMD_SPECIAL_PIPE,it means that pipfd[2] has been
-                                // initialized,	so it can be used directly
-                                dup2(pipfd[0], STDIN_FILENO);
-                                close(pipfd[0]);
-                        default:
-                                break;
-                        }
-
-                        // set the ouput source according the type of the current cmd
-                        switch (cmd->next_special_type) {
-                        case CMD_SPECIAL_AND:
-                        case CMD_SPECIAL_DEFAULT:
-                                break;
-                        case CMD_SPECIAL_PIPE:
-                                // if the type is pipe,then initialize the pipfd[2] and redirect the
-                                // output
-                                pipe(pipfd);
-                                dup2(pipfd[1], STDOUT_FILENO);
-                                close(pipfd[1]);
-                                break;
-                        case CMD_SPECIAL_LEFT_REDIR:
-                        case CMD_SPECIAL_RIGHT_REDIR:
-                        default:
-                                break;
-                        }
-
-                        // exec the command after the configuration
-                        exec_builtin_command(cmd->argv[0], cmd->argc, cmd->argv);
-
-                        // restore standard input and output after execution
-                        dup2(fd_in, STDIN_FILENO);
-                        dup2(fd_out, STDOUT_FILENO);
-                }
-                // extern command and other command are require fork a new process
-                else {
-                        // sigprocmask(SIG_BLOCK, &mask_chld, &prev_mask);
-                        printf("the %s command is not currently supported\n", cmd->argv[0]);
-                }
+        // built-in command are not require fork a child process
+        // set the input source according to the type of the previous cmd
+        switch (cmd->prev_special_type) {
+        case CMD_SPECIAL_AND:
+        case CMD_SPECIAL_DEFAULT:
+                // if the type is CMD_SPECIAL_AND or CMD_SPECIAL_DEFAULT,do nothing
+                break;
+        case CMD_SPECIAL_PIPE:
+                // if the type is CMD_SPECIAL_PIPE,it means that pipfd[2] has been
+                // initialized,	so it can be used directly
+                dup2(pipfd[0], STDIN_FILENO);
+                close(pipfd[0]);
+        default:
+                break;
         }
+
+        // set the ouput source according the type of the current cmd
+        switch (cmd->next_special_type) {
+        case CMD_SPECIAL_AND:
+        case CMD_SPECIAL_DEFAULT:
+                break;
+        case CMD_SPECIAL_PIPE:
+                // if the type is pipe,then initialize the pipfd[2] and redirect the
+                // output
+                pipe(pipfd);
+                dup2(pipfd[1], STDOUT_FILENO);
+                close(pipfd[1]);
+                break;
+        case CMD_SPECIAL_LEFT_REDIR:
+        case CMD_SPECIAL_RIGHT_REDIR:
+        default:
+                break;
+        }
+
+        // exec the command after the configuration
+        exec_builtin_command(cmd->argv[0], cmd->argc, cmd->argv);
+
+        // restore standard input and output after execution
+        dup2(fd_in, STDIN_FILENO);
+        dup2(fd_out, STDOUT_FILENO);
+
+        sigprocmask(SIG_BLOCK, &mask_prev, NULL);
+
         close(fd_in);
         close(fd_out);
+}
+
+// extern command and other command are require fork a new process
+void process_extern_command(struct cmd *cmd) {
+        sigset_t mask_all, mask_chld, mask_prev;
+        pid_t pid;
+        int backup_in_fd;
+
+        sigfillset(&mask_all);
+        sigemptyset(&mask_chld);
+        sigaddset(&mask_chld, SIGCHLD);
+
+        sigprocmask(SIG_BLOCK, &mask_chld, &mask_prev);
+
+        // set the input source according the type of the current cmd
+        switch (cmd->prev_special_type) {
+        case CMD_SPECIAL_AND:
+        case CMD_SPECIAL_DEFAULT:
+                // if the type is CMD_SPECIAL_AND or CMD_SPECIAL_DEFAULT,do nothing
+                break;
+        case CMD_SPECIAL_PIPE:
+                // if the type is CMD_SPECIAL_PIPE,it means that pipfd[2] has been
+                // initialized,	so it can be used directly
+                dup2(pipfd[0], STDIN_FILENO);
+                backup_in_fd = dup(STDIN_FILENO);
+                close(pipfd[0]);
+        default:
+                break;
+        }
+
+        // set the ouput source according the type of the current cmd
+        switch (cmd->next_special_type) {
+        case CMD_SPECIAL_AND:
+        case CMD_SPECIAL_DEFAULT:
+                break;
+        case CMD_SPECIAL_PIPE:
+                pipe(pipfd);
+                break;
+        case CMD_SPECIAL_LEFT_REDIR:
+        case CMD_SPECIAL_RIGHT_REDIR:
+        default:
+                break;
+        }
+
+        // exec the command
+        if ((pid = Fork()) > 0) {
+                sigprocmask(SIG_SETMASK, &mask_prev, NULL);
+
+                if (cmd->prev_special_type == CMD_SPECIAL_PIPE) {
+                        dup2(backup_in_fd, STDIN_FILENO);
+                        close(backup_in_fd);
+                }
+
+                if (cmd->next_special_type == CMD_SPECIAL_PIPE) {
+                        dup2(pipfd[1], STDOUT_FILENO);
+                }
+                close(pipfd[0]);
+                close(pipfd[1]);
+
+                if (cmd->cmd_type == CMD_POSITION_EXEC) {
+                        if (execv(cmd->argv[0], cmd->argv) < 0) {
+                                fprintf(stderr, "execv error:%s\n", strerror(errno));
+                                exit(0);
+                        }
+                } else if (cmd->cmd_type == CMD_POSITION_EXTERN) {
+                        if (execvp(cmd->argv[0], cmd->argv) < 0) {
+                                fprintf(stderr, "execvp error:%s\n", strerror(errno));
+                                exit(0);
+                        }
+                }
+        }
+        close(pipfd[1]);
+        if (backup_in_fd)
+                close(backup_in_fd);
+
+        sigprocmask(SIG_SETMASK, &mask_prev, NULL);
+}
+
+// execute the command accordign to struct cmd_list
+void exec_cmd(struct cmd_list *cmd_list) {
+        struct cmd *cmd;
+
+        // exec the command
+        for (cmd = cmd_list->head; cmd != NULL; cmd = cmd->next) {
+                if (cmd->argc == CMD_POSITION_BUILTIN)
+                        process_builtin_command(cmd);
+                else
+                        process_extern_command(cmd);
+        }
 }
 
 /*void eval(char *cmdline) {
