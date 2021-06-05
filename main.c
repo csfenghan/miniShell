bd6f87167b2768150e06906b7cd667bb564d22eb
fenghan <csfenghan@outlook.com>
2021-06-05 09:43:14 +0800

    修复了bug

--- main.c
+++ main.c
@@ -107,7 +107,7 @@ void process_extern_command(struct cmd *cmd) {
         }
 
         // exec the command
-        if ((pid = Fork()) > 0) {
+        if ((pid = Fork()) == 0) {
                 sigprocmask(SIG_SETMASK, &mask_prev, NULL);
 
                 if (cmd->prev_special_type == CMD_SPECIAL_PIPE) {
@@ -131,12 +131,10 @@ void process_extern_command(struct cmd *cmd) {
                                 fprintf(stderr, "execvp error:%s\n", strerror(errno));
                                 exit(0);
                         }
+                } else {
+                        printf("???");
                 }
         }
-        close(pipfd[1]);
-        if (backup_in_fd)
-                close(backup_in_fd);
-
         sigprocmask(SIG_SETMASK, &mask_prev, NULL);
 }
 
@@ -152,55 +150,6 @@ void exec_cmd(struct cmd_list *cmd_list) {
                         process_extern_command(cmd);
         }
 }
-
-/*void eval(char *cmdline) {
-        char *argv[MAXARGS];
-        char buf[MAXLINE];
-        int bg;
-        pid_t pid;
-        sigset_t mask_all, mask_chld, prev_mask;
-
-        strcpy(buf, cmdline);
-        bg = parse_line(buf, argv);
-
-        if (argv[0] == NULL) {
-                return;
-        }
-
-        if (!is_buildin_command(argv)) {
-                sigfillset(&mask_all);
-                sigemptyset(&mask_chld);
-                sigaddset(&mask_chld, SIGCHLD);
-
-                sigprocmask(SIG_BLOCK, &mask_chld, &prev_mask);
-                if ((pid = Fork()) == 0) {
-                        sigprocmask(SIG_SETMASK, &prev_mask, NULL);
-                        if (setpgid(0, 0) < 0)
-                                unix_error("setpgid error");
-
-                        if (execvp(argv[0], argv) < 0) {
-                                printf("%s: Command not found.\n", argv[0]);
-                                exit(0);
-                        }
-                }
-
-                sigprocmask(SIG_BLOCK, &mask_all, NULL);
-                if (!bg) {
-                        tcsetpgrp(STDIN_FILENO, pid);
-                        add_job(jobs, pid, F_R, cmdline);
-                        flags = 0;
-                        while (!flags)
-                                sigsuspend(&prev_mask);
-                        tcsetpgrp(STDIN_FILENO, getpid());
-
-                } else {
-                        add_job(jobs, pid, B_R, cmdline);
-                        printf("[%d]  %d %s\n", pid2jid(jobs, pid), pid, cmdline);
-                }
-                sigprocmask(SIG_SETMASK, &prev_mask, NULL);
-        }
-}*/
-
 int main(int argc, char *argv[]) {
         char cmdline[MAXLINE];
         struct cmd_list *cmd_list;
@@ -212,13 +161,16 @@ int main(int argc, char *argv[]) {
         Signal(SIGINT, sigint_handler);
         Signal(SIGTSTP, sigtstp_handler);
         Signal(SIGQUIT, sigquit_handler);
-        Signal(SIGCHLD, sigchld_handler);
+        // Signal(SIGCHLD, sigchld_handler);
 
         while (1) {
                 // waiting for user input
                 printf("%s>", getcwd(NULL, 0));
                 fflush(stdout);
-                Fgets(cmdline, MAXLINE, stdin);
+                if (fgets(cmdline, MAXLINE, stdin) == NULL) {
+                        perror("fgets error");
+                        exit(0);
+                }
                 if (feof(stdin))
                         exit(0);
 
