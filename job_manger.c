29b3d95761ec0e9ebf3aac71a31c3a653dda77be
fenghan <csfenghan@outlook.com>
2021-06-06 15:26:51 +0800

    修改了job管理

--- job_manger.c
+++ job_manger.c
@@ -1,129 +1,61 @@
 #include "job_manger.h"
-#include <unix_api.h>
 
-int next_jid;
+static struct job_t *job_list = NULL, *curr_job = NULL;
+static int next_jid = 0;
 
-/* 作业控制函数start */
-void init_jobs(struct job_t *job_list) {
-        for (int i = 0; i < MAX_JOBS; i++) {
-                job_list[i].pid = -1;
-                job_list[i].jid = -1;
-                job_list[i].state = UDF;
-        }
-        next_jid = 0;
-}
-
-void add_job(struct job_t *job_list, pid_t pid, int state, char *cmdline) {
-        job_list[next_jid].pid = pid;
-        job_list[next_jid].jid = next_jid + 1;
-        job_list[next_jid].state = state;
-        strcpy(job_list[next_jid].cmdline, cmdline);
-
-        if (++next_jid >= MAX_JOBS)
-                next_jid = 0;
-}
-
-void del_job(struct job_t *job_list, pid_t jid) {
-        if (job_list[jid - 1].jid == jid) {
-                job_list[jid - 1].pid = -1;
-                job_list[jid - 1].jid = -1;
-                job_list[jid - 1].state = UDF;
-
-                if (next_jid == jid)
-                        next_jid--;
-        }
-}
-
-void list_jobs(struct job_t *job_list) {
-        char *state;
-
-        for (int i = 0; i < next_jid; i++) {
-                if (job_list[i].pid != -1) {
-                        switch (job_list[i].state) {
-                        case UDF:
-                                state = "Undefined";
-                                break;
-                        case F_R:
-                                state = "F Running";
-                                break;
-                        case B_R:
-                                state = "B Running";
-                                break;
-                        case B_S:
-                                state = "B Stopped";
-                                break;
-                        default:
-                                state = "State error";
-                                break;
-                        }
-                        printf("[%d]  %s  %s", job_list[i].jid, state, job_list[i].cmdline);
-                        fflush(stdout);
-                }
-        }
-}
-
-pid_t get_fg_job(struct job_t *job_list) {
-
-        for (int i = 0; i < next_jid; i++)
-                if (job_list[i].state == F_R)
-                        return job_list[i].pid;
-        return -1;
-}
-
-pid_t jid2pid(struct job_t *job_list, pid_t jid) { return job_list[jid].pid; }
-
-pid_t pid2jid(struct job_t *job_list, pid_t pid) {
-
-        for (int i = 0; i < next_jid; i++) {
-                if (job_list[i].pid == pid)
-                        return job_list[i].jid;
-        }
-        return -1;
-}
-
-struct job_t *jid2job(struct job_t *job_list, pid_t jid) {
-        if (jid <= 0 || jid > MAX_JOBS)
-                return NULL;
-        return &job_list[jid - 1];
-}
-
-struct job_t *pid2job(struct job_t *job_list, pid_t pid) {
-        pid_t jid;
-
-        if ((jid = pid2jid(job_list, pid)) != -1)
-                return &job_list[jid - 1];
-        return NULL;
-}
-
-void do_fgbg(char *argv[]) {
-        pid_t jid;
-        sigset_t mask_all, mask_prev;
+struct job_t *create_job() {
         struct job_t *job;
 
-        if (argv[1] == NULL)
-                jid = next_jid;
-        else
-                jid = atoi(argv[1]);
+        job = Malloc(sizeof(struct job_t));
 
-        if (((job = jid2job(jobs, jid)) == NULL) ||
-            job->pid == -1) { /* 如果作业号无效，则报错返回 */
-                fprintf(stderr, "fg: %d:no such job\n", jid);
-                return;
+        job->jid = -1;
+        job->process_count = 0;
+        job->done_count = 0;
+
+        return job;
+}
+
+void add_process(struct job_t *job, pid_t pid, char *cmdline, enum process_state state) {
+        if (job->process_head == NULL) {
+                job->process_head = Malloc(sizeof(struct process_info));
+                job->process_tail = job->process_head;
+        } else {
+                job->process_tail->next =
+                    (struct process_info *)Malloc(sizeof(struct process_info));
+                job->process_tail = job->process_tail->next;
         }
 
-        /* 发送信号启动进程，前台等待进程 */
-        sigfillset(&mask_all);
-        sigprocmask(SIG_BLOCK, &mask_all, &mask_prev);
-        kill(job->pid, SIGCONT); /* 最好是通过返回的SIGCHLD信号来判断是否运行，这样至少图省事*/
-        if (strcmp(argv[0], "fg") == 0) {
-                tcsetpgrp(STDIN_FILENO, job->pid);
-                flags = 0; /* 等待前台作业 */
-                job->state = F_R;
-                while (!flags)
-                        sigsuspend(&mask_prev);
-                tcsetpgrp(STDIN_FILENO, getpid());
-        } else if (strcmp(argv[0], "bg") == 0) {
-                job->state = B_R;
+        job->process_tail->pid = pid;
+        job->process_tail->state = state;
+        job->process_tail->next = NULL;
+        strcpy(job->process_tail->cmdline, cmdline);
+
+        job->process_count++;
+}
+void add_job(struct job_t *job, char *cmdline) {
+        job->jid = ++next_jid;
+
+        if (job_list == NULL) {
+                job_list = job;
+                curr_job = job;
+        } else {
+                curr_job->next = job;
+                curr_job = curr_job->next;
         }
-        sigprocmask(SIG_SETMASK, &mask_prev, NULL);
+}
+
+void list_job() {
+        struct job_t *curr;
+
+        curr = job_list;
+        while (curr != NULL) {
+                printf("[%d]", curr->jid);
+                struct process_info *info = curr->process_head;
+                while (info != NULL) {
+                        printf(" running %s\n", info->cmdline);
+                        info = info->next;
+                }
+
+                curr = curr->next;
+        }
 }
--- job_manger.h
+++ job_manger.h
@@ -1,35 +1,38 @@
-#ifndef MINI_SHELL_H
-#define MINI_SHELL_H
+#ifndef JOB_MANGER_H
+#define JOB_MANGER_H
 
-#include <sys/types.h>
-#include <unix_api.h>
+#include "unix_api.h"
 
-enum job_state {
-        UDF = 0,
-        F_R = 1, // forground running
-        B_R = 2, // background running
-        B_S = 3, // background stop
+enum process_state {
+        UNDEFINE = 0,
+        FORGROUND_RUNNING = 1,
+        BACKGROUND_RUNNING = 2,
+        STOP = 3,
+        DONE = 4,
 };
 
-#define MAX_JOBS 1024
-struct job_t {
+struct process_info {
         pid_t pid;
-        pid_t jid;
-        enum job_state state;
         char cmdline[MAXLINE];
+        enum process_state state;
+        struct process_info *next;
 };
-int flags;
-struct job_t jobs[MAX_JOBS];
 
-void init_jobs(struct job_t *job_list);
-void add_job(struct job_t *job_list, pid_t pid, int state, char *cmdline);
-void del_job(struct job_t *job_list, pid_t jid);
-void list_jobs(struct job_t *job_list);
-pid_t get_fg_job(struct job_t *job_list);
-pid_t jid2pid(struct job_t *job_list, pid_t jid);
-pid_t pid2jid(struct job_t *job_list, pid_t pid);
-struct job_t *jid2job(struct job_t *job_list, pid_t jid);
-struct job_t *pid2job(struct job_t *job_list, pid_t pid);
-void do_fgbg(char *argv[]);
+struct job_t {
+        int jid;
+        int process_count;
+        int done_count;
+        struct process_info *process_head;
+        struct process_info *process_tail;
+        char cmdline[MAXLINE];
+	struct job_t *next;
+};
+
+struct job_t *create_job();
+void add_process(struct job_t *job,pid_t pid,char *cmdline,enum process_state state);
+void add_job(struct job_t *job,char *cmdline);
+void list_job();
+
+void del_process();
 
 #endif
