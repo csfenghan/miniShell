fac7a804cac11378349874dca52a5c9eeae1d95b
fenghan <csfenghan@outlook.com>
2021-06-06 16:40:26 +0800

    修改了chld处理函数

--- signal_handler.c
+++ signal_handler.c
@@ -1,21 +1,24 @@
-#include <unix_api.h>
 #include "job_manger.h"
+#include <unix_api.h>
+
+int is_forground_running = 1;
 
 /* 信号处理函数start */
 void sigint_handler(int sig) {
-        int old_errno;
-        pid_t pid;
+        /*        int old_errno;
+                pid_t pid;
 
-        old_errno = errno;
-        pid = get_fg_job(jobs);
-        if (pid != -1) {
-                kill(pid, sig);
-        }
-        errno = old_errno;
+                old_errno = errno;
+                pid = get_fg_job(jobs);
+                if (pid != -1) {
+                        kill(pid, sig);
+                }
+                errno = old_errno;
+                */
 }
 
 void sigtstp_handler(int sig) {
-        int old_errno;
+        /*int old_errno;
         pid_t pid;
 
         old_errno = errno;
@@ -25,24 +28,25 @@ void sigtstp_handler(int sig) {
         }
         errno = old_errno;
         return;
+        */
 }
 
 void sigquit_handler(int sig) {
-        int old_errno;
-        pid_t pid;
+        /*        int old_errno;
+                pid_t pid;
 
-        old_errno = errno;
-        pid = get_fg_job(jobs);
-        if (pid != -1) {
-                kill(pid, sig);
-        }
-        errno = old_errno;
-        return;
+                old_errno = errno;
+                pid = get_fg_job(jobs);
+                if (pid != -1) {
+                        kill(pid, sig);
+                }
+                errno = old_errno;
+                return;
+                */
 }
 
 void sigchld_handler(int sig) {
         pid_t pid, fg_pid;
-        struct job_t *job;
         int status, old_errno;
         sigset_t mask_all, mask_prev;
 
@@ -50,31 +54,22 @@ void sigchld_handler(int sig) {
         sigfillset(&mask_all);
         sigprocmask(SIG_BLOCK, &mask_all, &mask_prev);
 
-        fg_pid = get_fg_job(jobs);
+        // fg_pid = get_fg_job(jobs);
         pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED);
 
-        if (WIFEXITED(status)) { /*正常退出*/
-                if (fg_pid == pid)
-                        flags = 1;
-                del_job(jobs, pid2jid(jobs, pid));
+        if (WIFEXITED(status) || WIFSIGNALED(status)) {
+                if (del_process(pid) == 1)
+                        is_forground_running = 0;
 
-        } else if (WIFSIGNALED(status)) { /*被信号杀死*/
-                if (fg_pid == pid)
-                        flags = 1;
-                del_job(jobs, pid2jid(jobs, pid));
+        } else if (WIFSTOPPED(status)) { // stop
+                // if (fg_pid == pid)
+                //       flags = 1;
+                // job = pid2job(jobs, pid);
+                // job->state = B_S;
 
-        } else if (WIFSTOPPED(status)) { /*进程停止*/
-                if (fg_pid == pid)
-                        flags = 1;
-                job = pid2job(jobs, pid);
-                job->state = B_S;
-
-        } else if (WIFCONTINUED(status)) { /* 接受并打印continue信息，kill发送者负责更改jobs */
+        } else if (WIFCONTINUED(status)) {
         }
 
         sigprocmask(SIG_SETMASK, &mask_prev, NULL);
         errno = old_errno;
 }
-/* 信号处理函数end */
-
-
