66737b29ec01fca26a84d601cda3dbc33380131d
fenghan <csfenghan@foxmail.com>
2021-06-07 15:12:06 +0800

    修改了前台作业

--- job_manger.c
+++ job_manger.c
@@ -4,128 +4,139 @@ static struct job_t *job_head = NULL, *job_tail = NULL;
 static int next_jid = 0;
 
 void init_job() {
-        job_head = Malloc(sizeof(struct job_t));
-        job_tail = Malloc(sizeof(struct job_t));
-        job_head->prev = NULL;
-        job_head->next = job_tail;
-        job_tail->prev = job_head;
-        job_tail->next = NULL;
+	job_head = Malloc(sizeof(struct job_t));
+	job_tail = Malloc(sizeof(struct job_t));
+	job_head->prev = NULL;
+	job_head->next = job_tail;
+	job_tail->prev = job_head;
+	job_tail->next = NULL;
 }
 
 struct job_t *create_job() {
-        struct job_t *job;
+	struct job_t *job;
 
-        job = Malloc(sizeof(struct job_t));
+	job = Malloc(sizeof(struct job_t));
 
-        job->jid = -1;
-        job->process_count = 0;
-        job->done_count = 0;
-        job->prev = NULL;
-        job->next = NULL;
+	job->jid = -1;
+	job->process_count = 0;
+	job->done_count = 0;
+	job->prev = NULL;
+	job->next = NULL;
 
-        return job;
+	return job;
 }
 
 void add_process(struct job_t *job, pid_t pid, char *cmdline, enum process_state state) {
-        if (job->process_head == NULL) {
-                job->process_head = Malloc(sizeof(struct process_info));
-                job->process_tail = job->process_head;
-        } else {
-                job->process_tail->next =
-                    (struct process_info *)Malloc(sizeof(struct process_info));
-                job->process_tail = job->process_tail->next;
-        }
+	if (job->process_head == NULL) {
+		job->process_head = Malloc(sizeof(struct process_info));
+		job->process_tail = job->process_head;
+	} else {
+		job->process_tail->next =
+			(struct process_info *)Malloc(sizeof(struct process_info));
+		job->process_tail = job->process_tail->next;
+	}
 
-        job->process_tail->pid = pid;
-        job->process_tail->state = state;
-        job->process_tail->next = NULL;
-        strcpy(job->process_tail->cmdline, cmdline);
+	job->process_tail->pid = pid;
+	job->process_tail->state = state;
+	job->process_tail->next = NULL;
+	strcpy(job->process_tail->cmdline, cmdline);
 
-        job->process_count++;
+	job->process_count++;
 }
 void add_job(struct job_t *job, char *cmdline) {
-        job->jid = ++next_jid;
+	job->jid = ++next_jid;
 
-        job->prev = job_tail->prev;
-        job->next = job_tail;
-        job_tail->prev->next = job;
-        job_tail->prev = job;
+	job->prev = job_tail->prev;
+	job->next = job_tail;
+	job_tail->prev->next = job;
+	job_tail->prev = job;
 }
 
 void list_job() {
-        struct job_t *curr;
+	struct job_t *curr;
 
-        curr = job_head->next;
-        while (curr != job_tail) {
-                printf("[%d]\n", curr->jid);
-                struct process_info *info = curr->process_head;
-                while (info != NULL) {
-                        char *state;
-                        switch (info->state) {
-                        case FORGROUND_RUNNING:
-                                state = "f_running";
-                                break;
-                        case BACKGROUND_RUNNING:
-                                state = "b_running";
-                                break;
-                        case STOP:
-                                state = "stop";
-                                break;
-                        case DONE:
-                                state = "done";
-                                break;
-                        case UNDEFINE:
-                        default:
-                                state = "undefine";
-                                break;
-                        }
-                        printf("    %s    %s\n", state, info->cmdline);
-                        info = info->next;
-                }
+	curr = job_head->next;
+	while (curr != job_tail) {
+		printf("[%d]\n", curr->jid);
+		struct process_info *info = curr->process_head;
+		while (info != NULL) {
+			char *state;
+			switch (info->state) {
+				case FORGROUND_RUNNING:
+					state = "f_running";
+					break;
+				case BACKGROUND_RUNNING:
+					state = "b_running";
+					break;
+				case STOP:
+					state = "stop";
+					break;
+				case DONE:
+					state = "done";
+					break;
+				case UNDEFINE:
+				default:
+					state = "undefine";
+					break;
+			}
+			printf("    %s    %s\n", state, info->cmdline);
+			info = info->next;
+		}
 
-                curr = curr->next;
-        }
+		curr = curr->next;
+	}
 }
 
-static void destroy_job(struct job_t *job) {
-        struct process_info *info;
+void destroy_job(struct job_t *job) {
+	struct process_info *info;
 
-        job->prev->next = job->next;
-        job->next->prev = job->prev;
+	job->prev->next = job->next;
+	job->next->prev = job->prev;
 
-        // free the process_info list
-        info = job->process_head;
-        while (info != NULL) {
-                struct process_info *temp = info;
-                info = info->next;
-                free(temp);
-        }
+	// free the process_info list
+	info = job->process_head;
+	while (info != NULL) {
+		struct process_info *temp = info;
+		info = info->next;
+		free(temp);
+	}
 
-        if (next_jid == job->jid)
-                next_jid--;
-        // free the job_t
-        free(job);
+	if (next_jid == job->jid)
+		next_jid--;
+	// free the job_t
+	free(job);
 }
 
-// return 1 if a job is deleted,else retrun 0
+// return jid if a job is deleted,else retrun 0
 int del_process(pid_t pid) {
-        struct job_t *job;
-        struct process_info *info;
+	struct job_t *job;
+	struct process_info *info;
+	int result;
 
-        for (job = job_head->next; job != job_tail; job = job->next) {
-                for (info = job->process_head; info != NULL; info = info->next) {
-                        if (info->pid == pid) {
-                                goto found;
-                        }
-                }
-        }
-        unix_error("del process error,not found pid!");
+	for (job = job_head->next; job != job_tail; job = job->next) {
+		for (info = job->process_head; info != NULL; info = info->next) {
+			if (info->pid == pid) {
+				goto found;
+			}
+		}
+	}
+	unix_error("del process error,not found pid!");
 
 found:
-        info->state = DONE;
-        if (++(job->done_count) == job->process_count) {
-                destroy_job(job);
-                return 1;
-        }
-        return 0;
+	result=0;
+	info->state = DONE;
+	if (++(job->done_count) == job->process_count) {
+		result=job->jid;
+		destroy_job(job);
+	}
+	return result;
+}
+int get_fg_job(){
+	struct job_t *job;
+	for(job=job_head->next;job!=job_tail;job=job->next){
+		if(job->process_head->state==FORGROUND_RUNNING)
+			return job->jid;
+	}
+
+	return -1;
 }
--- job_manger.h
+++ job_manger.h
@@ -4,27 +4,27 @@
 #include "unix_api.h"
 
 enum process_state {
-        UNDEFINE = 0,
-        FORGROUND_RUNNING = 1,
-        BACKGROUND_RUNNING = 2,
-        STOP = 3,
-        DONE = 4,
+	UNDEFINE = 0,
+	FORGROUND_RUNNING = 1,
+	BACKGROUND_RUNNING = 2,
+	STOP = 3,
+	DONE = 4,
 };
 
 struct process_info {
-        pid_t pid;
-        char cmdline[MAXLINE];
-        enum process_state state;
-        struct process_info *next;
+	pid_t pid;
+	char cmdline[MAXLINE];
+	enum process_state state;
+	struct process_info *next;
 };
 
 struct job_t {
-        int jid;
-        int process_count;
-        int done_count;
-        struct process_info *process_head;
-        struct process_info *process_tail;
-        char cmdline[MAXLINE];
+	int jid;
+	int process_count;	// total number of processes in this job
+	int done_count;		// total number of completed processes in the job
+	struct process_info *process_head;	
+	struct process_info *process_tail;
+	char cmdline[MAXLINE];
 
 	struct job_t *prev;
 	struct job_t *next;
@@ -35,6 +35,8 @@ struct job_t *create_job();
 void add_process(struct job_t *job,pid_t pid,char *cmdline,enum process_state state);
 void add_job(struct job_t *job,char *cmdline);
 void list_job();
+void destroy_job(struct job_t *job) ;
+int get_fg_job();
 
 int del_process(pid_t pid);
 
