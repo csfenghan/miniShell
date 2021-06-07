00ed5ab9d48ea4925c9bdbd6dff407ef8a6e02e8
fenghan <csfenghan@foxmail.com>
2021-06-07 15:12:16 +0800

    修改了sigchld

--- signal_handler.c
+++ signal_handler.c
@@ -1,76 +1,76 @@
 #include "job_manger.h"
 #include <unix_api.h>
 
-int is_forground_running = 1;
+int forground_jid= 1;
 
 /* 信号处理函数start */
 void sigint_handler(int sig) {
-        /*        int old_errno;
-                pid_t pid;
+	/*        int old_errno;
+		  pid_t pid;
 
-                old_errno = errno;
-                pid = get_fg_job(jobs);
-                if (pid != -1) {
-                        kill(pid, sig);
-                }
-                errno = old_errno;
-                */
+		  old_errno = errno;
+		  pid = get_fg_job(jobs);
+		  if (pid != -1) {
+		  kill(pid, sig);
+		  }
+		  errno = old_errno;
+		  */
 }
 
 void sigtstp_handler(int sig) {
-        /*int old_errno;
-        pid_t pid;
+	/*int old_errno;
+	  pid_t pid;
 
-        old_errno = errno;
-        pid = get_fg_job(jobs);
-        if (pid != -1) {
-                kill(pid, sig);
-        }
-        errno = old_errno;
-        return;
-        */
+	  old_errno = errno;
+	  pid = get_fg_job(jobs);
+	  if (pid != -1) {
+	  kill(pid, sig);
+	  }
+	  errno = old_errno;
+	  return;
+	  */
 }
 
 void sigquit_handler(int sig) {
-        /*        int old_errno;
-                pid_t pid;
+	/*        int old_errno;
+		  pid_t pid;
 
-                old_errno = errno;
-                pid = get_fg_job(jobs);
-                if (pid != -1) {
-                        kill(pid, sig);
-                }
-                errno = old_errno;
-                return;
-                */
+		  old_errno = errno;
+		  pid = get_fg_job(jobs);
+		  if (pid != -1) {
+		  kill(pid, sig);
+		  }
+		  errno = old_errno;
+		  return;
+		  */
 }
 
 void sigchld_handler(int sig) {
-        pid_t pid, fg_pid;
-        int status, old_errno;
-        sigset_t mask_all, mask_prev;
+	pid_t pid;
+	int status, old_errno;
+	sigset_t mask_all, mask_prev;
 
-        old_errno = errno;
-        sigfillset(&mask_all);
-        sigprocmask(SIG_BLOCK, &mask_all, &mask_prev);
+	old_errno = errno;
+	sigfillset(&mask_all);
+	sigprocmask(SIG_BLOCK, &mask_all, &mask_prev);
 
-        // fg_pid = get_fg_job(jobs);
-        pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED);
+	forground_jid= get_fg_job();
+	while((pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED))>0){
+		if (WIFEXITED(status) || WIFSIGNALED(status)) {
+			if (del_process(pid) == forground_jid)
+				forground_jid= 0;
+			printf("pid:%d normally exit\n", pid);
 
-        if (WIFEXITED(status) || WIFSIGNALED(status)) {
-                if (del_process(pid) == 1)
-                        is_forground_running = 0;
-                printf("pid:%d normally exit\n", pid);
+		} else if (WIFSTOPPED(status)) { // stop
+			// if (fg_pid == pid)
+			//       flags = 1;
+			// job = pid2job(jobs, pid);
+			// job->state = B_S;
 
-        } else if (WIFSTOPPED(status)) { // stop
-                // if (fg_pid == pid)
-                //       flags = 1;
-                // job = pid2job(jobs, pid);
-                // job->state = B_S;
+		} else if (WIFCONTINUED(status)) {
+		}
+	}
 
-        } else if (WIFCONTINUED(status)) {
-        }
-
-        sigprocmask(SIG_SETMASK, &mask_prev, NULL);
-        errno = old_errno;
+	sigprocmask(SIG_SETMASK, &mask_prev, NULL);
+	errno = old_errno;
 }
